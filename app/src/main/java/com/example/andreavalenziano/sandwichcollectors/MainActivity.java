a2ca844bf8dd70cb1a4b866a3fe97bfb9a2692e0
AndreaValenziano <valenzianoa@gmail.com>
2017-03-02 21:53:59 +0100

    mainactivity

---
+++ app/src/main/java/com/example/andreavalenziano/sandwichcollectors/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.andreavalenziano.sandwichcollectors;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AndreaValenziano on 02/03/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
