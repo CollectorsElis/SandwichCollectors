ebd4aa80db64a781560f6fc0a2db3e9877077b85
RobertoLentini <roberto.lentini@live.com>
2017-03-03 13:20:51 +0100

    Navigation Drawer

--- app/src/main/java/com/example/andreavalenziano/sandwichcollectors/activities/FiguListActivity.java
+++ app/src/main/java/com/example/andreavalenziano/sandwichcollectors/activities/FiguListActivity.java
@@ -25,7 +25,7 @@ public class FiguListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_figu_list);
+        setContentView(R.layout.content_figu_list);
     }
 
     @Override
--- app/src/main/java/com/example/andreavalenziano/sandwichcollectors/activities/MainActivity.java
+++ app/src/main/java/com/example/andreavalenziano/sandwichcollectors/activities/MainActivity.java
@@ -2,78 +2,126 @@ package com.example.andreavalenziano.sandwichcollectors.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.example.andreavalenziano.sandwichcollectors.R;
 
-import static android.content.ContentValues.TAG;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
-/**
- * Created by AndreaValenziano on 02/03/17.
- */
+    Button toListBtn, toChangeAreaBtn;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button toListBtn, toChargeAreaBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toListBtn = (Button) findViewById(R.id.to_list_btn);
-        toChargeAreaBtn = (Button) findViewById(R.id.to_change_area_btn);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        toListBtn.setOnClickListener(this);
-        toChargeAreaBtn.setOnClickListener(this);
+        // set ascoltatore sui bottoni
+        toListBtn=(Button)findViewById(R.id.to_list_btn);
+       toListBtn.setOnClickListener(this);
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+
 
     @Override
     public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this,FiguListActivity.class));
 
-        if (toListBtn != null && v.getId() == R.id.to_list_btn) {
-            startActivity(new Intent(MainActivity.this, FiguListActivity.class));
-        }
-        if (toChargeAreaBtn != null && v.getId() == R.id.to_change_area_btn) {
-            //go to change area activity
-
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
 
     }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart");
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
-
 }
